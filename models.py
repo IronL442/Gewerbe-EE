0fed6a874189f0b0aa9e5ebdef2de0f5e31eb822
Laurits <l.eisengarten98@googlemail.com>
2025-03-16 12:01:41 +0100

    Create models.py
    Create models.py to store database models

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class StudySession(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    student_name = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    session_topic = db.Column(db.String(200), nullable=False)
+    signature_path = db.Column(db.String(255), nullable=False)  # Path to signature image
