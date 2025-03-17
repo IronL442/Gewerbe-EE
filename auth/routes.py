d03a7a891912e97c728142a6f34dc85246712ec4
Laurits <l.eisengarten98@googlemail.com>
2025-03-17 17:49:59 +0100

    Remove user registration functionality

--- auth/routes.py
+++ auth/routes.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, login_required, logout_user, current_user
-from werkzeug.security import check_password_hash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user
 from models import User
+from app import bcrypt  # Import bcrypt for password checking
 
-# Define authentication blueprint
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -11,12 +10,16 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.password, password):
+        
+        # Fetch the only user from the database
+        user = User.query.first()
+
+        if user and username == user.username and bcrypt.check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('sessions.dashboard'))
         else:
             flash('Invalid username or password', 'danger')
+
     return render_template('login.html')
 
 @auth.route('/logout')
