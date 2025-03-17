dfdc255041169d0fc337e859f1e9bd8db74abf4e
Laurits <l.eisengarten98@googlemail.com>
2025-03-17 09:03:39 +0100

    Fix login

--- app.py
+++ app.py
@@ -3,15 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+from models import User  # Import the User model
 
 # Initialize Flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/study_sessions'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@localhost:5432/enterprise-db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'auth.login'
+
+# Flask-Login User Loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))  # Ensure User model is imported
 
 # Ensure signature folder exists
 if not os.path.exists('static/signatures'):
