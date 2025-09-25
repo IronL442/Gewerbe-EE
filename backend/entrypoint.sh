a96c9002bf9869ac1e5c429ea86d3e18643c7209
Laurits <l.eisengarten98@googlemail.com>
2025-09-25 16:12:47 +0200

    Update Entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -24,4 +24,9 @@ while [ $attempt -le $MAX_RETRIES ]; do
 done
 
 echo "Starting gunicorn..."
-exec gunicorn "app:app" --workers "${WORKERS:-2}" --threads "${THREADS:-8}" --preload --bind "0.0.0.0:${PORT:-8000}"
+exec gunicorn "app:app" \
+    --workers "${WORKERS:-2}" \
+    --threads "${THREADS:-8}" \
+    --preload \
+    --bind "0.0.0.0:${PORT:-8000}" \
+    --bind "[::]:${PORT:-8000}"
